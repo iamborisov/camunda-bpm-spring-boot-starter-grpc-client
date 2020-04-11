29855cee61d577b76b059b0a6bd99e9fd349a99b
borisov <a.borisov@8bitgroup.com>
2020-04-11 05:21:12 +0300

    variables from map

--- src/main/java/org/camunda/bpm/engine/grpc/client/domain/Variables.java
+++ src/main/java/org/camunda/bpm/engine/grpc/client/domain/Variables.java
@@ -1,9 +1,26 @@
 package org.camunda.bpm.engine.grpc.client.domain;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Value;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Value
 public class Variables {
 
     String value;
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static Variables from(Map<String, Object> data) throws JsonProcessingException {
+        return new Variables(
+            mapper.writeValueAsString(
+                data.entrySet().stream()
+                    .filter(v -> !Objects.isNull(v.getValue()))
+                    .collect(Collectors.toMap(e -> e, Map.Entry::getValue)))
+        );
+    }
 }
